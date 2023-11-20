a3b8a9b86a470c13ac0f8541f9053598d05a5044
Hadassah Uche <uhadassah1@gmail.com>
2023-11-20 21:51:41 +0200

    Add project files.

---
+++ UcheHadassah6_6/HadassahUche6_6.cpp
@@ -0,0 +1,6 @@
+/*Merge: Write a program to merge the contents of two given words, so that letters are 
+taken alternatively from one word and then from the other. The program should create 
+a string with those letters and display it on the screen. (When strings are of unequal 
+lengths then the remaining letters of the longest string are appended at the end).
+For example, assuming that the user enters the two words: “cat” and “horse” then the 
+resulting, merged string that is displayed on the screen is: “chaotrse”*/
