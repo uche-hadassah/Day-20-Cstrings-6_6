36417eab519f405742e500cb7d2d233fcc3b6ee4
Hadassah Uche <uhadassah1@gmail.com>
2023-11-20 22:25:33 +0200

    Cleaned up the code and added comments

--- UcheHadassah6_6/HadassahUche6_6.cpp
+++ UcheHadassah6_6/HadassahUche6_6.cpp
@@ -1,9 +1,5 @@
-/*Merge: Write a program to merge the contents of two given words, so that letters are 
-taken alternatively from one word and then from the other. The program should create 
-a string with those letters and display it on the screen. (When strings are of unequal 
-lengths then the remaining letters of the longest string are appended at the end).
-For example, assuming that the user enters the two words: “cat” and “horse” then the 
-resulting, merged string that is displayed on the screen is: “chaotrse”*/
+/*Name: Uche Hadassah
+This program merges the contents of two given words alternatively and appends lefover words at the end*/
 #include<iostream>
 #include<cstring>
 using namespace std;
@@ -15,23 +11,26 @@ int main()
 	cin >> str1;
 	cout << "Enter the second word:";
 	cin >> str2;
-	for (size1 = 0; str1[size1] != '\0'; size1++);
-	for (size2 = 0; str2[size2] != '\0'; size2++);
-	int maxsize = size1;
+
+	for (size1 = 0; str1[size1] != '\0'; size1++);//gets the length of the string just like "strlen (str1);"
+	for (size2 = 0; str2[size2] != '\0'; size2++);//gets the length of the string just like "strlen (str2);"
+	int maxsize = size1;//Initialize a maxsize to one of the string lengths
+
 	if(size1 != size2)
 	{
 		if (size2 > size1)
 		{
-			maxsize = size2;
+			maxsize = size2;//changes if the other is bigger
 		}
 	}
+
 	for (int i = 0; i < maxsize; i++)
 	{
-		if (i < size1)
+		if (i < size1)//Outputs until i !< size1
 		{
 			cout << str1[i];
 		}
-		if (i < size2)
+		if (i < size2)//Outputs until i !< size2
 		{
 			cout << str2[i];
 		}
